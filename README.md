4bdeb5bdd69b514c9406a8ae87e7996c9c9e57f8
mukuljainmj <mukuljain0295@gmail.com>
2025-04-23 10:13:37 +0530

    React JS project setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # interview-preparation
+
+<!--
+# React JS Project Setup Link -
+https://www.notion.so/Setup-React-JS-project-1de2e042860580558ac9dba5a0594396
+
+#
+ -->
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function App() {
+  return <div>App</div>;
+}
+
+root.render(<App></App>);
